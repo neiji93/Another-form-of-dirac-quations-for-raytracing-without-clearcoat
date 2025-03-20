1e2c7fc62e17c16a3e6158899dd99c7bfa980634
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-03-20 11:29:08 +0100

    Create AKModelFirstMeanFieldGaleCanceller.hlsl

---
+++ AKModelFirstMeanFieldGaleCanceller.hlsl
@@ -0,0 +1,12 @@
+//as clearcoat is bot pbr and as we can use
+//zny mixed models, we can start to cancel clearcoat
+/)shading with a basic first trapped clearcoat valur in scene shading
+//then use shadowing.
+
+
+//thr cpp program would need to patch
+//rays paths
+
+
+//the dirac esuation is not needed here but we can 
+//enforce ...
